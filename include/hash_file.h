04c8f3eb623278db622ce98fc7f41f8e4d1be863
lampisgaganis <162498899+lampisgaganis@users.noreply.github.com>
2024-03-07 11:32:17 +0200

    Add files via upload

---
+++ include/hash_file.h
@@ -0,0 +1,98 @@
+#ifndef HASH_FILE_H
+#define HASH_FILE_H
+
+typedef enum HT_ErrorCode {
+  HT_OK,
+  HT_ERROR
+} HT_ErrorCode;
+
+typedef struct Record {
+	int id;
+	char name[15];
+	char surname[20];
+	char city[20];
+} Record;
+
+typedef struct HT_Info {
+	int fd;
+	int depth;            	//HT TOTAL DEPTH
+	int rec_per_block;    	//RECORDS PER BLOCK = (BLOCK SIZE - one int to count records in block)/RECORD SIZE
+	int HT_block_capacity;	//CAPACITY OF HT BLOCKS = BLOCK SIZE/INT SIZE
+						  	//HT BLOCKS HOLD BLOCK IDS OF THE BLOCKS ACTING AS THE HASH TABLE BUCKETS
+	int* HT;				// Hash Table
+	//for statistics
+	int block_count;		// Total record count
+	int min_rec_in_bucket;	// Minimum records in a bucket
+	int max_rec_in_bucket;	// Maximum records in a bucket
+	int rec_count;			// Total record count
+	int bucket_count;		// Bucket count (data blocks)
+
+} HT_info;
+
+/*
+ * Η συνάρτηση HT_Init χρησιμοποιείται για την αρχικοποίηση κάποιον δομών που μπορεί να χρειαστείτε. 
+ * Σε περίπτωση που εκτελεστεί επιτυχώς, επιστρέφεται HT_OK, ενώ σε διαφορετική περίπτωση κωδικός λάθους.
+ */
+HT_ErrorCode HT_Init();
+
+/*
+ * Η συνάρτηση HT_CreateIndex χρησιμοποιείται για τη δημιουργία και κατάλληλη αρχικοποίηση ενός άδειου αρχείου κατακερματισμού με όνομα fileName. 
+ * Στην περίπτωση που το αρχείο υπάρχει ήδη, τότε επιστρέφεται ένας κωδικός λάθους. 
+ * Σε περίπτωση που εκτελεστεί επιτυχώς επιστρέφεται HΤ_OK, ενώ σε διαφορετική περίπτωση κωδικός λάθους.
+ */
+HT_ErrorCode HT_CreateIndex(
+	const char *fileName,		/* όνομααρχείου */
+	int depth
+	);
+
+
+/*
+ * Η ρουτίνα αυτή ανοίγει το αρχείο με όνομα fileName. 
+ * Εάν το αρχείο ανοιχτεί κανονικά, η ρουτίνα επιστρέφει HT_OK, ενώ σε διαφορετική περίπτωση κωδικός λάθους.
+ */
+HT_ErrorCode HT_OpenIndex(
+	const char *fileName, 		/* όνομα αρχείου */
+  int *indexDesc            /* θέση στον πίνακα με τα ανοιχτά αρχεία  που επιστρέφεται */
+	);
+
+/*
+ * Η ρουτίνα αυτή κλείνει το αρχείο του οποίου οι πληροφορίες βρίσκονται στην θέση indexDesc του πίνακα ανοιχτών αρχείων.
+ * Επίσης σβήνει την καταχώρηση που αντιστοιχεί στο αρχείο αυτό στον πίνακα ανοιχτών αρχείων. 
+ * Η συνάρτηση επιστρέφει ΗΤ_OK εάν το αρχείο κλείσει επιτυχώς, ενώ σε διαφορετική σε περίπτωση κωδικός λάθους.
+ */
+HT_ErrorCode HT_CloseFile(
+	int indexDesc 		/* θέση στον πίνακα με τα ανοιχτά αρχεία */
+	);
+
+/*
+ * Η συνάρτηση HT_InsertEntry χρησιμοποιείται για την εισαγωγή μίας εγγραφής στο αρχείο κατακερματισμού. 
+ * Οι πληροφορίες που αφορούν το αρχείο βρίσκονται στον πίνακα ανοιχτών αρχείων, ενώ η εγγραφή προς εισαγωγή προσδιορίζεται από τη δομή record. 
+ * Σε περίπτωση που εκτελεστεί επιτυχώς επιστρέφεται HT_OK, ενώ σε διαφορετική περίπτωση κάποιος κωδικός λάθους.
+ */
+HT_ErrorCode HT_InsertEntry(
+	int indexDesc,	/* θέση στον πίνακα με τα ανοιχτά αρχεία */
+	Record record		/* δομή που προσδιορίζει την εγγραφή */
+	);
+
+/*
+ * Η συνάρτηση HΤ_PrintAllEntries χρησιμοποιείται για την εκτύπωση όλων των εγγραφών που το record.id έχει τιμή id. 
+ * Αν το id είναι NULL τότε θα εκτυπώνει όλες τις εγγραφές του αρχείου κατακερματισμού. 
+ * Σε περίπτωση που εκτελεστεί επιτυχώς επιστρέφεται HP_OK, ενώ σε διαφορετική περίπτωση κάποιος κωδικός λάθους.
+ */
+HT_ErrorCode HT_PrintAllEntries(
+	int indexDesc,	/* θέση στον πίνακα με τα ανοιχτά αρχεία */
+	int *id 				/* τιμή του πεδίου κλειδιού προς αναζήτηση */
+	);
+
+/*
+ * Η συνάρτηση διαβάζει το αρχείο με όνομα filename και τυπώνει στατιστικά στοιχεία: 
+ * α)Το πόσα blocks έχει ένα αρχείο, 
+ * β)Το ελάχιστο, το μέσο και το μέγιστο πλήθος εγγραφών που έχει κάθε bucket ενός αρχείου.
+ * Σε περίπτωση που εκτελεστεί επιτυχώς επιστρέφεται HT_OK, ενώ σε διαφορετική περίπτωση κάποιος κωδικός λάθους. 
+*/
+HT_ErrorCode HashStatistics(
+	char* filename /* όνομα του αρχείου που ενδιαφέρει */ 
+	);
+
+
+#endif // HASH_FILE_H
